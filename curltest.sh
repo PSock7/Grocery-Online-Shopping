7235477d649ec0efdfe5fea4f090288eda5d1f3e
xDev <papesock@yahoo.fr>
2023-12-11 15:33:30 +0100

    shopping yaml file

--- curltest.sh
+++ curltest.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
-curl --header "Content-Type: application/json" --request POST --data '{ "email":"test4@test.com",  "password":"12345", "phone":"12345"}' http://localhost:3501/signup
-curl --header "Content-Type: application/json" --request POST --data '{"email":"test4@test.com",  "password":"12345"}' http://localhost:3501/login
+#curl --header "Content-Type: application/json" --request POST --data '{ "email":"test4@test.com",  "password":"12345", "phone":"12345"}' http://localhost:3501/signup
+customer_url=$(minikube service customer-node-service --url)
+modified_url="${customer_url}"
 
+# Assuming you want to use the modified URL in your curl command
+curl --header "Content-Type: application/json" --request POST --data '{"email":"test4@test.com",  "password":"12345"}' "$modified_url/login"
